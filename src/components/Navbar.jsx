1fc67bb83a95c6273af31a417f9b62a1c0861b67
Shrisant Adhikari <Shreesantadhikari4590@gmail.com>
2025-03-31 16:33:37 +0545

    add second

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css'; // Import the CSS file for styling
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${isOpen ? 'open' : ''}`}>
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">
           Shikha Shrestha
           <span className="logo-model">Model</span>
         </Link>
-        <ul className="navbar-menu">
+        <button className="hamburger" onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </button>
+        <ul className={`navbar-menu ${isOpen ? 'open' : ''}`}>
           <li className="navbar-item">
-            <Link to="/" className="navbar-link">
+            <Link to="/" className="navbar-link" onClick={() => setIsOpen(false)}>
               Home
             </Link>
           </li>
           <li className="navbar-item">
-            <Link to="/about" className="navbar-link">
+            <Link to="/about" className="navbar-link" onClick={() => setIsOpen(false)}>
               About
             </Link>
           </li>
           <li className="navbar-item">
-            <Link to="/contact" className="navbar-link">
+            <Link to="/contact" className="navbar-link" onClick={() => setIsOpen(false)}>
               Contact
             </Link>
           </li>
